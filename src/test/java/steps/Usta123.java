fa2cd31228722ea09c536bec07585070e1999943
arlan1907 <arslanurly1907@gmail.com>
2023-03-20 00:07:46 -0500

    Arslan added some code

--- src/test/java/steps/Usta123.java
+++ src/test/java/steps/Usta123.java
@@ -1,5 +1,23 @@
 package steps;
 
+
 public class Usta123 {
+    public static void main(String[] args) {
+        // Write a program that will print Fibonacci numbers between 1 and 100.
+        // 0 1 1 2 3 5 8 13 ....
+        int fib1 = 0;
+        int fib2 = 1;
+        int fibNext =0 ;
+
+        System.out.print(fib1 +" ");
+
+        while (fibNext<100) {
+            fibNext = fib1+fib2;
+            fib1=fib2;
+            fib2=fibNext;
+            System.out.print(fib1 + " ");
+        }
+    }
+
 
 }
