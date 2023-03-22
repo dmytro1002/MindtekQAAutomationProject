c63c0d2005b162d4d1d5d30c29105cfa313b5b27
mindtek <dmytrokostiuk13@gmail.com>
2023-03-21 14:40:09 -1000

    Added myCompany

--- src/test/java/steps/Dmytro.java
+++ src/test/java/steps/Dmytro.java
@@ -2,5 +2,10 @@ package steps;
 
 public class Dmytro {
 
+    public static void main(String[] args) {
 
+        String myCompany="Mindtek";
+
+        System.out.println("I'm working at "+myCompany);
+    }
 }
