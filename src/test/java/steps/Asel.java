3cb2fb10ffddc429668fefd946b58e93a719a0e8
As.A <97922504+AselAkhmatova@users.noreply.github.com>
2023-03-21 20:33:08 -0500

    Add company

--- src/test/java/steps/Asel.java
+++ src/test/java/steps/Asel.java
@@ -1,4 +1,11 @@
 package steps;
 
 public class Asel {
+
+    public static void main(String[] args) {
+
+        String company = "Google";
+
+        System.out.println("My company" + company);
+    }
 }
