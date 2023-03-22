19d195859f4b325cf060124c756d54af3166c1d1
Miranda1505 <mirumir1505@gmail.com>
2023-03-21 19:40:15 -0500

    Added myCompany

--- src/test/java/steps/Miranda.java
+++ src/test/java/steps/Miranda.java
@@ -1,5 +1,8 @@
 package steps;
 
 public class Miranda {
-
+    public static void main(String[] args) {
+        String myCompany="Chase";
+        System.out.println("I'm working at "+myCompany);
+    }
 }
