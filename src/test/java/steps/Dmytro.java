3b6b4b57f9e288c3a987d81ab19de13126a5adea
Shirin <shirinhudaybergenowa@.gmail.com>
2023-03-21 19:40:15 -0500

    Added myCompeny

--- src/test/java/steps/Dmytro.java
+++ src/test/java/steps/Dmytro.java
@@ -1,6 +1,12 @@
 package steps;
 
 public class Dmytro {
+    public static void main(String[] args) {
+
+        String myCompany = "SHIRIN";
+        System.out.println("I'm working at " + myCompany);
+
+    }
 
 
 }
