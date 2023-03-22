1a634bc0a5b961fef4b07a349361a4cb4fd925ee
omur28 <omurusta28@gmail.com>
2023-03-21 19:40:38 -0500

    Added myCompany omur

---
+++ src/test/java/steps/Omur.java
@@ -0,0 +1,10 @@
+package steps;
+
+public class Omur {
+    public static void main(String[] args) {
+
+        String myCompany = "My Company";
+
+        System.out.println("I'm working at "+myCompany);
+    }
+}
