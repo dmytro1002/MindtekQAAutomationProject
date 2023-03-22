d725c7ecb7ba29156a0bc458502d37d6054c91ba
As.A <97922504+AselAkhmatova@users.noreply.github.com>
2023-03-21 21:12:27 -0500

    Add cod

--- src/test/java/steps/Asel.java
+++ src/test/java/steps/Asel.java
@@ -2,7 +2,23 @@ package steps;
 
 public class Asel {
 
+    public static void armstrongNum(int num){
+        int sum = 0;
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, 3); // vosvedenie v stepen
+            temp /= 10;
+        }
+        if (sum == num) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+    }
+
     public static void main(String[] args) {
+        armstrongNum(154);
 
         String company = "Google ";
 
