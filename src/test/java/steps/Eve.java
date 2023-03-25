a14016fbee427d8aa4b2dbcb509eecd70eef5212
EveSou <evesouilem@gmail.com>
2023-03-25 04:17:39 -0400

    Company update

--- src/test/java/steps/Eve.java
+++ src/test/java/steps/Eve.java
@@ -2,5 +2,12 @@ package steps;
 
 public class Eve {
 
+    public static void main(String[] args) {
+
+        String myCompany = "Eve's Ruling";
+
+        System.out.println("I'm working at " + myCompany);
+    }
+
 
 }
